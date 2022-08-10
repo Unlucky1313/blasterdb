9e9493bfc55cc3a7c17c34eaf069be18ca153234
Unlucky1313 <dylanstandfast@gmail.com>
2022-08-10 13:03:09 -0500

    Breaking out code

---
+++ src/Description.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from "@mui/material/Card";
+
+export default function Description(props) {
+  return (
+    <>
+      <Card className="description">
+
+      </Card>
+    </>
+  );
+}
