869bac67661af7c530b9f4e677eebb60adc51b19
DStandfastBG <dstandfast@bgprod.com>
2023-10-30 16:08:21 -0500

    Polished Header Menu Button

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
@@ -7,13 +7,20 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { NavLink } from "react-router-dom";
 import { setDoc } from "firebase/firestore";
 
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import FeedbackIcon from "@mui/icons-material/Feedback";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import MenuList from "@mui/material/MenuList";
 
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 
@@ -24,7 +31,7 @@ var config = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
@@ -34,19 +41,49 @@ const auth = firebase.auth();
 
 export default function Header(props) {
   const [user] = useAuthState(auth);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+  const [userRole, setUserRole] = useState("User");
+
+  function changeRole(role){
+    setUserRole(role);
+  }
 
   function handleUser(value) {
     props.onChange(value);
   }
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
   };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
 
   return (
     <header className="App-header">
@@ -55,56 +92,112 @@ export default function Header(props) {
       </NavLink>
 
       <div className="sideHead">
-        <NavLink className="navLink" to="/search">
-          <SearchIcon />Search
-        </NavLink>
-        <NavLink className="navLink" to="/add">
-          <AddCircleIcon /> Add
-        </NavLink>
-        <NavLink className="navLink" to="/features">
-          Request A Feature!
-        </NavLink>
         <section className="profile">
           {user ? (
             <>
               <Profile />
-              <CheckForUser onChange={handleUser} />
+              <CheckForUser onChange={handleUser} changeRole={changeRole} />
             </>
           ) : (
             <SignIn />
           )}
         </section>
-      </div>
-      <div>
-        <Button
-          id="basic-button"
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
-          onClick={handleClick}
-          variant="contained"
-        >
-          Dashboard
-        </Button>
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
-          }}
-        >
-          <MenuItem>
-            <Button href="/search" onClick={handleClose} size="small" ><SearchIcon />Search</Button>
-          </MenuItem>
-          <MenuItem>
-            <Button href="/add" onClick={handleClose} size="small" ><AddCircleIcon />Add</Button>
-          </MenuItem>
-          <MenuItem>
-            <Button href="/features" onClick={handleClose} size="small" >Request a Feature!</Button>
-          </MenuItem>
-        </Menu>
+        <div className="menu">
+          <Button
+            ref={anchorRef}
+            id="composition-button"
+            aria-controls={open ? "composition-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+            variant="contained"
+            className="menuIcon"
+          >
+            <MenuIcon />
+          </Button>
+          <Popper
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            placement="bottom-start"
+            transition
+            disablePortal
+            sx={{zIndex: 100}}
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin:
+                    placement === "bottom-start" ? "left top" : "right top",
+                }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList
+                      autoFocusItem={open}
+                      id="composition-menu"
+                      aria-labelledby="composition-button"
+                      onKeyDown={handleListKeyDown}
+                    >
+                      <MenuItem className="menuItem">
+                        <Button
+                          href="/search"
+                          onClick={handleClose}
+                          size="small"
+                          className="menuButton"
+                        >
+                          <SearchIcon />
+                          Search
+                        </Button>
+                      </MenuItem>
+                      {(userRole === "Admin" || userRole === "Moderator") ? 
+                      <MenuItem className="menuItem">
+                        <Button
+                          href="/add"
+                          onClick={handleClose}
+                          size="small"
+                          className="menuButton"
+                        >
+                          <AddCircleIcon />
+                          Add a Blaster
+                        </Button>
+                      </MenuItem>
+                      : ""}
+                      <MenuItem className="menuItem">
+                        <Button
+                          href="/features"
+                          onClick={handleClose}
+                          size="small"
+                          className="menuButton"
+                        >
+                          <FeedbackIcon />
+                          Request a Feature!
+                        </Button>
+                      </MenuItem>
+                      {auth.currentUser ? (
+                        <MenuItem className="menuItem">
+                          <Button
+                            onClick={() => {
+                              auth.signOut(); 
+                              window.location.reload();
+                            }}
+                            className="menuButton"
+                          >
+                            <LogoutIcon />
+                            Sign Out
+                          </Button>
+                        </MenuItem>
+                      ) : (
+                        ""
+                      )}
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </div>
       </div>
     </header>
   );
@@ -136,16 +229,6 @@ function Profile() {
             }
           />
         </Link>
-        <Button
-          variant="contained"
-          onClick={() => auth.signOut()}
-          style={{
-            fontFamily:
-              "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-          }}
-        >
-          Sign Out
-        </Button>
       </>
     )
   );
@@ -162,10 +245,16 @@ function CheckForUser(props) {
     .then((doc) => {
       if (doc.exists) {
         props.onChange(uid);
+        props.changeRole(doc.data()["role"]);
       } else {
         // doc.data() will be undefined in this case
         props.onChange(uid);
-        setDoc(docRef, { collected: [], wishlist: [], username: email, role: "User" });
+        setDoc(docRef, {
+          collected: [],
+          wishlist: [],
+          username: email,
+          role: "User",
+        });
       }
     })
     .catch((error) => {
